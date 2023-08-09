e19be647b6718d0e551f5350bd4a9a7bbe9056a8
Albert Dang <98789245+albertdang8@users.noreply.github.com>
2023-08-09 16:31:34 -0700

    met with client :|

--- code/src/components/NavBar/NavBar.jsx
+++ code/src/components/NavBar/NavBar.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { FaPizzaSlice } from "react-icons/fa";
 import "./NavBar.css";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <ul>
-      <li class="dropdown">
-        <a href="" class="dropbtn">
-          <FaPizzaSlice />
-        </a>
-        <div className="dropdown-content">
-          <a href="#">About Bocca</a>
-          <a href="#">Contact</a>
-          <a href="#">Pizzas</a>
-        </div>
-      </li>
-    </ul>
+    <div className="nav-container">
+      <ul>
+        <li class="dropdown">
+          <a href="" className="dropbtn">
+            <FaPizzaSlice />
+          </a>
+          <div className="dropdown-content">
+            <Link to="#">Contact</Link>
+            <Link to="/about">About Bocca</Link>
+            <Link to="#">Pizzas</Link>
+          </div>
+        </li>
+      </ul>
+      <div className="title">
+        BoccaBoccaPizza
+      </div>
+    </div>
   );
 };
 
--- code/src/pages/HomePage/HomePage.jsx
+++ code/src/pages/HomePage/HomePage.jsx
@@ -5,11 +5,17 @@ import "./HomePage.css";
 const HomePage = () => {
   return (
     <div className="page">
-      <div className="one center">
-        <h1 className="title">BoccaBoccaPizza</h1>
+      <div className="section one center">
       </div>
-      <div className="two center">
+      <div className="section two center">
         Test
+      </div>
+      <div className="break">
+        <div className="section img1"></div>
+        <div className="section img2"></div>
+      </div>
+      <div className="section three center">
+        test
       </div>
     </div>
   );
