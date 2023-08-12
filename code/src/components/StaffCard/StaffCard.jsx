808c8e31e47eb45ed9e70f3c3af6df5ee942a672
Albert <albertdang8@gmail.com>
2023-08-12 00:19:43 -0700

    staff section re-imagined and mobile responsive

--- code/src/components/StaffCard/StaffCard.jsx
+++ code/src/components/StaffCard/StaffCard.jsx
@@ -2,65 +2,95 @@ import React from "react";
 import "./StaffCard.css";
 
 const StaffCard = () => {
-
   return (
-    <div className="staff-container">
-      <div className="staff-card">
-        <div className="img" />
-        <div className="staff-details">
-          <h2 className="staff-name">Tommy Beckelheimer</h2>
-          <p>
-            <strong>Favorite Movie:</strong> That Thing You Do
-          </p>
-          <p>
-            <strong>Favorite Music:</strong> Oso Oso, Dua Lipa, Spoon
-          </p>
-          <p>
-            <strong>Top Foods:</strong> Oysters, Anything Italian/Mediterranean
-          </p>
-          <p>
-            <strong>Quirk:</strong> Every dog reminds me of mine
-          </p>
-        </div>
+    <div className="container">
+      <div className="header">
+        <h1>Our Team</h1>
       </div>
-      <div className="staff-card">
-        <div className="img-dog" />
-        <div className="staff-details">
-          <h2 className="staff-name">Ravioli</h2>
-          <p>
-            <strong>Favorite Movie:</strong> Airbud
-          </p>
-          <p>
-            <strong>Favorite Music:</strong> Botch, Cake, Dua Lipa, Feist
-          </p>
-          <p>
-            <strong>Top Foods:</strong> Mine own asshole
-          </p>
-          <p>
-            <strong>Quirk:</strong> I poopie on floor lol
-          </p>
+      <div className="sub-container">
+        <div className="teams">
+          <div className="tommy" />
+          <div className="name">Tommy Beckelheimer</div>
+          <div className="desig">Top Cook Man</div>
+          <div className="about">
+            <p>
+              <strong>Favorite Movie:</strong> <br /> That Thing You Do
+            </p>
+            <p>
+              <strong>Favorite Music:</strong> <br /> Oso Oso, Dua Lipa, Spoon
+            </p>
+            <p>
+              <strong>Top Foods:</strong> <br /> Oysters, Anything
+              Italian/Mediterranean
+            </p>
+            <p>
+              <strong>Quirk:</strong> <br /> Every dog reminds me of mine
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="staff-card">
-        <div className="img-bert" />
-        <div className="staff-details">
-          <h2 className="staff-name">Albert</h2>
-          <p>
-            <strong>Favorite Movie:</strong> Casablanca
-          </p>
-          <p>
-            <strong>Favorite Music:</strong> Botch, Cake, Dua Lipa, Feist
-          </p>
-          <p>
-            <strong>Top Foods:</strong> Hotdogs, Foie gras, Chinese, Chicken McNuggets, Chalupa
-          </p>
-          <p>
-            <strong>Quirk:</strong> I'm the piece of shit that made this website.
-          </p>
+
+        <div className="teams">
+          <div className="ravioli" />
+          <div className="name">Ravioli Beckelheimer</div>
+          <div className="desig">Family Specialist Dog-ctor</div>
+          <div className="about">
+            <p>
+              <strong>Favorite Movie:</strong> <br /> Airbud
+            </p>
+            <p>
+              <strong>Favorite Music:</strong> <br /> Baha Men, Johann Sebastian Bark
+            </p>
+            <p>
+              <strong>Top Foods:</strong> <br /> Mine own asshole
+            </p>
+            <p>
+              <strong>Quirk:</strong> <br /> I make the poopie on floor lol
+            </p>
+          </div>
         </div>
+
+        <div className="teams">
+          <div className="bert profile-pic" />
+          <div className="name">Albert</div>
+          <div className="desig">Edge Lord</div>
+          <div className="about">
+            <p>
+              <strong>Favorite Movie:</strong> <br /> Casablanca
+            </p>
+            <p>
+              <strong>Favorite Music:</strong> <br /> Botch, Dua Lipa, Feist
+            </p>
+            <p>
+              <strong>Top Foods:</strong> <br /> Hotdogs, Foie Gras, Chicken McNuggets, Chalupas
+            </p>
+            <p>
+              <strong>Quirk:</strong> <br /> I'm the shit-bag that made this site
+            </p>
+          </div>
+        </div>
+
+        {/* <div className="teams">
+          <div className="tommy" />
+          <div className="name">Tommy Beckelheimer</div>
+          <div className="desig">Top Cook Man</div>
+          <div className="about">
+            <p>
+              <strong>Favorite Movie:</strong> <br /> That Thing You Do
+            </p>
+            <p>
+              <strong>Favorite Music:</strong> <br /> Oso Oso, Dua Lipa, Spoon
+            </p>
+            <p>
+              <strong>Top Foods:</strong> <br /> Oysters, Anything
+              Italian/Mediterranean
+            </p>
+            <p>
+              <strong>Quirk:</strong> <br /> Every dog reminds me of mine
+            </p>
+          </div>
+        </div> */}
       </div>
     </div>
-    
   );
 };
 
