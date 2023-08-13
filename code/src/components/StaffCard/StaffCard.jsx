c08d7dd55f409322147c60098eedddf4efeb5f1a
Albert <albertdang8@gmail.com>
2023-08-13 03:15:36 -0700

    smash and pass

--- code/src/components/StaffCard/StaffCard.jsx
+++ code/src/components/StaffCard/StaffCard.jsx
@@ -5,7 +5,12 @@ const StaffCard = () => {
   return (
     <div className="container">
       <div className="header">
-        <h1>Our Team</h1>
+        <h1>Our Staff</h1>
+        <p className="summary">
+          We believe in giving credit where credit is due. Here, you'll see some
+          of the faces of the friends and workers who have helped us become what
+          we are today.
+        </p>
       </div>
       <div className="sub-container">
         <div className="teams">
@@ -38,7 +43,8 @@ const StaffCard = () => {
               <strong>Favorite Movie:</strong> <br /> Airbud
             </p>
             <p>
-              <strong>Favorite Music:</strong> <br /> Baha Men, Johann Sebastian Bark
+              <strong>Favorite Music:</strong> <br /> Baha Men, Johann Sebastian
+              Bark
             </p>
             <p>
               <strong>Top Foods:</strong> <br /> Mine own asshole
@@ -61,15 +67,17 @@ const StaffCard = () => {
               <strong>Favorite Music:</strong> <br /> Botch, Dua Lipa, Feist
             </p>
             <p>
-              <strong>Top Foods:</strong> <br /> Hotdogs, Foie Gras, Chicken McNuggets, Chalupas
+              <strong>Top Foods:</strong> <br /> Hotdogs, Foie Gras, Chicken
+              McNuggets, Chalupas
             </p>
             <p>
-              <strong>Quirk:</strong> <br /> I'm the shit-bag that made this site
+              <strong>Quirk:</strong> <br /> I'm the shit-bag that made this
+              site
             </p>
           </div>
         </div>
 
-        <div className="teams">
+        {/* <div className="teams">
           <div className="tommy" />
           <div className="name">Tommy Beckelheimer</div>
           <div className="desig">Top Cook Man</div>
@@ -106,6 +114,34 @@ const StaffCard = () => {
             </p>
             <p>
               <strong>Quirk:</strong> <br /> Every dog reminds me of mine
+            </p>
+          </div>
+        </div> */}
+      </div>
+
+      <div className="header">
+        <h1>The Artists</h1>
+      </div>
+      <div className="sub-container">
+        <div className="teams">
+          <div className="preemo" />
+          <div className="name">Priscilla Moreno</div>
+          <div className="desig">Logo Artist</div>
+          <div className="about">
+            <p>
+              <strong>About:</strong> <br /> Priscilla is the designer of our
+              logo. Her work has been featured all over the Orange County/Los
+              Angeles area and continues to spread life and wonderment wherever
+              she may be.
+            </p>
+            <p>
+              <strong>Site:</strong> <br />{" "}
+              <a className="site-link" href="https://www.priscillamoreno.com/">
+                https://www.priscillamoreno.com/
+              </a>
+            </p>
+            <p>
+              <strong>Email:</strong> <br /> hi@priscillamoreno.com
             </p>
           </div>
         </div>
--- code/src/pages/HomePage/HomePage.jsx
+++ code/src/pages/HomePage/HomePage.jsx
@@ -4,18 +4,12 @@ import "./HomePage.css";
 
 const HomePage = () => {
   return (
-    <div className="page">
-      <div className="section one center">
-      </div>
-      <div className="section two center">
-        Test
-      </div>
-      <div className="break">
-        <div className="section img1"></div>
-        <div className="section img2"></div>
-      </div>
-      <div className="section three center">
-        test
+    <div>
+      <div className="banner-container">
+        <div className="banner-left">
+
+        </div>
+        
       </div>
     </div>
   );
