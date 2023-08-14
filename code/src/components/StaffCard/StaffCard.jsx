fc978d0514c26b69cf2ba5108e2e606176f08865
Albert Dang <98789245+albertdang8@users.noreply.github.com>
2023-08-13 19:24:35 -0700

    more about page shenaniganz

--- code/src/components/StaffCard/StaffCard.jsx
+++ code/src/components/StaffCard/StaffCard.jsx
@@ -7,14 +7,18 @@ const StaffCard = () => {
       <div className="header">
         <h1>Our Staff</h1>
         <p className="summary">
-          We believe in giving credit where credit is due. Here, you'll see some
-          of the faces of the friends and workers who have helped us become what
-          we are today.
+          We believe in giving credit where credit is due. That is to say,
+          you'll see us proudly displaying the faces of the very human friends
+          and workers who have helped us become what we are today. This page
+          does not exhaust the list of those to whom we extend our gratitude— 
+          including those who have given us business advice, regulars and their
+          cheerful disposition, family and loved ones who have given us endless
+          support.
         </p>
       </div>
       <div className="sub-container">
         <div className="teams">
-          <div className="tommy" />
+          <div className="team-image tommy" />
           <div className="name">Tommy Beckelheimer</div>
           <div className="desig">Top Cook Man</div>
           <div className="about">
@@ -35,7 +39,7 @@ const StaffCard = () => {
         </div>
 
         <div className="teams">
-          <div className="ravioli" />
+          <div className="team-image ravioli" />
           <div className="name">Ravioli Beckelheimer</div>
           <div className="desig">Family Specialist Dog-ctor</div>
           <div className="about">
@@ -56,12 +60,13 @@ const StaffCard = () => {
         </div>
 
         <div className="teams">
-          <div className="bert profile-pic" />
+          <div className="team-image bert" />
           <div className="name">Albert</div>
           <div className="desig">Edge Lord</div>
           <div className="about">
             <p>
-              <strong>Favorite Movie:</strong> <br /> Casablanca
+              <strong>Favorite Movie:</strong> <br /> Eurovision Song Contest:
+              The Story of Fire Saga
             </p>
             <p>
               <strong>Favorite Music:</strong> <br /> Botch, Dua Lipa, Feist
@@ -77,54 +82,79 @@ const StaffCard = () => {
           </div>
         </div>
 
-        {/* <div className="teams">
-          <div className="tommy" />
-          <div className="name">Tommy Beckelheimer</div>
-          <div className="desig">Top Cook Man</div>
+        <div className="teams">
+          <div className="team-image bobby" />
+          <div className="name">Bobby</div>
+          <div className="desig">4</div>
           <div className="about">
             <p>
-              <strong>Favorite Movie:</strong> <br /> That Thing You Do
+              <strong>Favorite Movie:</strong> <br /> Anything by FakeTaxi
             </p>
             <p>
-              <strong>Favorite Music:</strong> <br /> Oso Oso, Dua Lipa, Spoon
+              <strong>Favorite Music:</strong> <br /> The Beatles, Jimmy
+              Hendrix, Bill Evans
             </p>
             <p>
-              <strong>Top Foods:</strong> <br /> Oysters, Anything
-              Italian/Mediterranean
+              <strong>Top Foods:</strong> <br /> Gatorade
             </p>
             <p>
-              <strong>Quirk:</strong> <br /> Every dog reminds me of mine
+              <strong>Quirk:</strong> <br /> I like jazz... like a lot.
             </p>
           </div>
         </div>
         <div className="teams">
-          <div className="tommy" />
-          <div className="name">Tommy Beckelheimer</div>
-          <div className="desig">Top Cook Man</div>
+          <div className="team-image evan" />
+          <div className="name">Evan</div>
+          <div className="desig">5</div>
           <div className="about">
             <p>
-              <strong>Favorite Movie:</strong> <br /> That Thing You Do
+              <strong>Favorite Movie:</strong> <br /> Test
             </p>
             <p>
-              <strong>Favorite Music:</strong> <br /> Oso Oso, Dua Lipa, Spoon
+              <strong>Favorite Music:</strong> <br /> Test
             </p>
             <p>
-              <strong>Top Foods:</strong> <br /> Oysters, Anything
-              Italian/Mediterranean
+              <strong>Top Foods:</strong> <br /> Test
             </p>
             <p>
-              <strong>Quirk:</strong> <br /> Every dog reminds me of mine
+              <strong>Quirk:</strong> <br /> Test
             </p>
           </div>
-        </div> */}
+        </div>
+
+        <div className="teams">
+          <div className="team-image evan" />
+          <div className="name">Jacob</div>
+          <div className="desig">5</div>
+          <div className="about">
+            <p>
+              <strong>Favorite Movie:</strong> <br /> Test
+            </p>
+            <p>
+              <strong>Favorite Music:</strong> <br /> Test
+            </p>
+            <p>
+              <strong>Top Foods:</strong> <br /> Test
+            </p>
+            <p>
+              <strong>Quirk:</strong> <br /> Test
+            </p>
+          </div>
+        </div>
       </div>
 
       <div className="header">
         <h1>The Artists</h1>
+        <p className="summary">
+          These talented artists have played a pivotal role in shaping our
+          company's distinct image and function. Their creative contributions
+          have pushed our brand to have a unique and captivating visual
+          identity, setting us apart in a competitive landscape.
+        </p>
       </div>
       <div className="sub-container">
         <div className="teams">
-          <div className="preemo" />
+          <div className="team-image preemo" />
           <div className="name">Priscilla Moreno</div>
           <div className="desig">Logo Artist</div>
           <div className="about">
@@ -142,6 +172,51 @@ const StaffCard = () => {
             </p>
             <p>
               <strong>Email:</strong> <br /> hi@priscillamoreno.com
+            </p>
+          </div>
+        </div>
+        <div className="teams">
+          <div className="team-image a-frame" />
+          <div className="name">A-frame.placeholder</div>
+          <div className="desig">A-Frame Carving</div>
+          <div className="about">
+            <p>
+              <strong>About:</strong> <br /> This is the homie who carved our
+              beautiful a-frame sign. The signal to the fans that we're open for
+              business is when this frame is flipped out and placed in front of
+              our tables.
+            </p>
+            <p>
+              <strong>Site:</strong> <br />{" "}
+              <a className="site-link" href="#">
+                https://www.placeHolder.com/
+              </a>
+            </p>
+            <p>
+              <strong>Email:</strong> <br /> place@Holder.com
+            </p>
+          </div>
+        </div>
+        <div className="teams">
+          <div className="team-image placeholder" />
+          <div className="name">Photographer.placeHolder</div>
+          <div className="desig">Photographer</div>
+          <div className="about">
+            <p>
+              <strong>About:</strong> <br /> Tommy is absolutely terrible at
+              capturing the nuance of our work, so we're eternally grateful for
+              the blessing/contributions of user.placeHolder and their talents.
+              An essential part of our aesthetic relies upon the contributions
+              of our selfless photographers.
+            </p>
+            <p>
+              <strong>Site:</strong> <br />{" "}
+              <a className="site-link" href="#">
+                https://www.placeHolder.com/
+              </a>
+            </p>
+            <p>
+              <strong>Email:</strong> <br /> place@Holder.com
             </p>
           </div>
         </div>
--- code/src/pages/HomePage/HomePage.jsx
+++ code/src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 
+import bannerImage from "../../assets/Photos-001/DSC05910.jpg"
 import "./HomePage.css";
 
 const HomePage = () => {
   return (
     <div>
       <div className="banner-container">
-        <div className="banner-left">
-
+          <div className="banner">
+            <img src={bannerImage} alt="Banner" />
+          </div>
+        <div className="overlay">
         </div>
-        
       </div>
+
     </div>
   );
 };
