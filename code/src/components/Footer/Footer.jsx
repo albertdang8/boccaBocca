611f1f81443a28e1f83e93c5d7528a0946599d5e
Albert <albertdang8@gmail.com>
2023-09-02 18:45:26 -0700

    build for netlify started. site still incomplete by a lot

--- code/src/components/Footer/Footer.jsx
+++ code/src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <footer className="footer container section">
-      <h3 className="footer__title">~Bocca Bocca Pizza~</h3>
+      <h3 className="footer__title">*~Bocca Bocca Pizza~*</h3>
     </footer>
   )
 }
